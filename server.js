163c4b9aab6e184f89fef24b747e75dec4200386
Rakesh Sinha <rakeshsinha31@gmail.com>
2021-03-21 20:54:45 +0530

    setup(server): initial commit, check connectivity to server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+  res.send("test connctiviti to server");
+});
+
+const startServer = (port) => {
+  app.listen(port);
+};
+
+startServer(Number(3000));
